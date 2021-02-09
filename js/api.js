ad0e44324980862f6d8011569404a6b3a91e64ec
Ilya Ballet <iballet@uclan.ac.uk>
2021-02-09 11:41:06 +0200

    fixed request not displaying

--- js/api.js
+++ js/api.js
@@ -2,6 +2,7 @@ async function APIlist()
 {
     const response = await fetch("https://codecyprus.org/th/api/list");
     const reply = await response.text();
+    document.getElementById("list").innerText = reply;
 }
 
 APIlist();
