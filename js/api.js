7527fbf091604df2c36bf825c5f88fb7728d6dd0
Ilya Ballet <iballet@uclan.ac.uk>
2021-02-09 11:34:46 +0200

    added proper api request

--- js/api.js
+++ js/api.js
@@ -1,1 +1,7 @@
-let APIlist = JSON.parse("https://codecyprus.org/th/api/list");
+async function APIlist()
+{
+    const response = await fetch("https://codecyprus.org/th/api/list");
+    const reply = await response.text();
+}
+
+APIlist();
