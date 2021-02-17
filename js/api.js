66121d67bc794ce76dd74b173340280e980edd2a
Ilya Ballet <iballet@uclan.ac.uk>
2021-02-17 02:10:17 +0200

    fixed js not running properly

--- js/api.js
+++ js/api.js
@@ -1,16 +1,17 @@
+const APIdomain = "https://codecyprus.org/th/api/";
+
 async function ListTreasures()
 {
-    let response = await fetch("https://codecyprus.org/th/api/list");
+    let response = await fetch(APIdomain + "list");
 
     if (response.ok)
     {
         let reply = await response.json();
-        document.getElementById("list").innerText = "The list of Treasure Hunts:" + "\n\n" + reply.treasureHunts[0].name + "\n" + reply.treasureHunts[1].name;
+        document.getElementById("list").innerText = "The list of Treasure Hunts:" + "\n\n"
+        + reply.treasureHunts[0].name + "\n"
+        + reply.treasureHunts[1].name + "\n\n";
     }
-    else
-    {
-        alert("HTTP-Error: " + response.status);
-    }
+    else alert("HTTP-Error: " + response.status);
 }
 
 ListTreasures();
